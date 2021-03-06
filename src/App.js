d350ff24f59778fb9ba24ecf75f282adfd54a502
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2021-03-06 13:08:54 -0500

    finished file foundation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import EmployeeTable from './components/Table';
+import EmployeeTable from './components/EmployeeTable';
+
+// material imports
+import Container from '@material-ui/core/Container';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <Container fixed>
+                <EmployeeTable />
+            </Container>
+        </>
+    );
 }
 
 export default App;
